af28797160afc8d62e1e4b137fa88f09f89a72d5
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-03-30 22:21:35 +0530

    Add `PinnitBottomBar`

---
+++ app/src/main/java/dev/sasikanth/pinnit/activity/PinnitBottomBar.kt
@@ -0,0 +1,12 @@
+package dev.sasikanth.pinnit.activity
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class PinnitBottomBar @JvmOverloads
+constructor(
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr)
