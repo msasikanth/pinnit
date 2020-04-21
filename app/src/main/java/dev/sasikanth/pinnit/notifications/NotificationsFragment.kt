245cf7fec38e431e9c7666b63631a4c6910316d7
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-04-21 22:42:41 +0530

    Add `NotificationsFragment`

---
+++ app/src/main/java/dev/sasikanth/pinnit/notifications/NotificationsFragment.kt
@@ -0,0 +1,6 @@
+package dev.sasikanth.pinnit.notifications
+
+import androidx.fragment.app.Fragment
+import dev.sasikanth.pinnit.R
+
+class NotificationsFragment : Fragment(R.layout.fragment_notifications)
