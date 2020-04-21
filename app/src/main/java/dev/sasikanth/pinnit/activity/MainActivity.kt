383fde2572b531b5281fc09f6739cc80c04ea3fa
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-04-21 22:08:51 +0530

    Add toolbar to `MainActivity`

--- app/src/main/java/dev/sasikanth/pinnit/activity/MainActivity.kt
+++ app/src/main/java/dev/sasikanth/pinnit/activity/MainActivity.kt
@@ -3,7 +3,9 @@ package dev.sasikanth.pinnit.activity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.updatePadding
 import dev.sasikanth.pinnit.R
+import dev.sasikanth.pinnit.utils.donOnApplyWindowInsets
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     super.onCreate(savedInstanceState)
 
     mainRoot.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
+    appBarLayout.donOnApplyWindowInsets { view, windowInsets, initialPadding ->
+      view.updatePadding(top = windowInsets.systemWindowInsetTop + initialPadding.top)
+    }
+    setSupportActionBar(toolbar)
   }
 }
