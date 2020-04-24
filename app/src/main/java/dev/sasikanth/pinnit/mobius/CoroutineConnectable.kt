6a257606f7470782512694ae997b0e3d0b6dd46b
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-04-24 19:31:36 +0530

    Add `CoroutinesConnectable` base class

---
+++ app/src/main/java/dev/sasikanth/pinnit/mobius/CoroutineConnectable.kt
@@ -0,0 +1,40 @@
+package dev.sasikanth.pinnit.mobius
+
+import com.spotify.mobius.Connectable
+import com.spotify.mobius.Connection
+import com.spotify.mobius.functions.Consumer
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+abstract class CoroutineConnectable<I, O>(
+  private val dispatcher: CoroutineDispatcher
+) : Connectable<I, O>, CoroutineScope {
+
+  private val parentJob = SupervisorJob()
+  override val coroutineContext: CoroutineContext
+    get() = parentJob + dispatcher
+
+  abstract suspend fun handler(effect: I, dispatchEvent: (O) -> Unit)
+
+  @ExperimentalCoroutinesApi
+  override fun connect(output: Consumer<O>): Connection<I> {
+    val job = Job()
+    val scope = CoroutineScope(coroutineContext + job)
+
+    return object : Connection<I> {
+      override fun accept(input: I) {
+        scope.launch { handler(input, output::accept) }
+      }
+
+      override fun dispose() {
+        job.cancel()
+        parentJob.cancel()
+      }
+    }
+  }
+}
