3df1b5beaa145c47cbdd3927272e9270a23c29fd
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-04-26 18:16:00 +0530

    Launch deep link to editor screen on notification click

--- app/src/main/java/dev/sasikanth/pinnit/system/NotificationUtil.kt
+++ app/src/main/java/dev/sasikanth/pinnit/system/NotificationUtil.kt
@@ -3,15 +3,14 @@ package dev.sasikanth.pinnit.system
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.navigation.NavDeepLinkBuilder
 import dev.sasikanth.pinnit.R
-import dev.sasikanth.pinnit.activity.MainActivity
 import dev.sasikanth.pinnit.data.PinnitNotification
+import dev.sasikanth.pinnit.editor.EditorScreenArgs
 
 object NotificationUtil {
 
@@ -33,10 +32,11 @@ object NotificationUtil {
   private fun buildSystemNotification(context: Context, notification: PinnitNotification): Notification {
     val content = notification.content.orEmpty()
 
-    val intent = Intent(context, MainActivity::class.java).apply {
-      flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-    }
-    val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+    val pendingIntent = NavDeepLinkBuilder(context)
+      .setGraph(R.navigation.main_nav_graph)
+      .setDestination(R.id.editorScreen)
+      .setArguments(EditorScreenArgs(notification.uuid.toString()).toBundle())
+      .createPendingIntent()
 
     val builder = NotificationCompat.Builder(context, CHANNEL_ID)
       .setSmallIcon(R.drawable.ic_pinnit_notification)
