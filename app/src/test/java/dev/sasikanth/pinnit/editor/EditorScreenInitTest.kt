4f2713f5df9ce5b9de6fd45b6ee66f9b770741f0
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-04-26 11:31:16 +0530

    When screen is created and uuid is not present then do nothing

--- app/src/test/java/dev/sasikanth/pinnit/editor/EditorScreenInitTest.kt
+++ app/src/test/java/dev/sasikanth/pinnit/editor/EditorScreenInitTest.kt
@@ -2,6 +2,7 @@ package dev.sasikanth.pinnit.editor
 
 import com.spotify.mobius.test.FirstMatchers.hasEffects
 import com.spotify.mobius.test.FirstMatchers.hasModel
+import com.spotify.mobius.test.FirstMatchers.hasNoEffects
 import com.spotify.mobius.test.InitSpec
 import com.spotify.mobius.test.InitSpec.assertThatFirst
 import org.junit.Test
@@ -9,9 +10,10 @@ import java.util.UUID
 
 class EditorScreenInitTest {
 
+  private val initSpec = InitSpec(EditorScreenInit())
+
   @Test
   fun `when screen is created and notification uuid is present, then fetch notification`() {
-    val initSpec = InitSpec(EditorScreenInit())
     val notificationUuid = UUID.fromString("21221df8-4e51-4c26-8258-199a93ec2c04")
     val defaultModel = EditorScreenModel.default(notificationUuid)
 
@@ -21,6 +23,20 @@ class EditorScreenInitTest {
         assertThatFirst(
           hasModel(defaultModel),
           hasEffects(LoadNotification(notificationUuid) as EditorScreenEffect)
+        )
+      )
+  }
+
+  @Test
+  fun `when screen is created and notification uuid is not present, then do nothing`() {
+    val defaultModel = EditorScreenModel.default(null)
+
+    initSpec
+      .whenInit(defaultModel)
+      .then(
+        assertThatFirst(
+          hasModel(defaultModel),
+          hasNoEffects()
         )
       )
   }
