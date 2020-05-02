026e11b0f6cd89e39abc196f4648fc550b79af52
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-05-02 06:53:40 +0530

    Refactor `CoroutineConnectable` to set proper coroutine scope

--- app/src/main/java/dev/sasikanth/pinnit/mobius/CoroutineConnectable.kt
+++ app/src/main/java/dev/sasikanth/pinnit/mobius/CoroutineConnectable.kt
@@ -5,7 +5,6 @@ import com.spotify.mobius.Connection
 import com.spotify.mobius.functions.Consumer
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
@@ -14,24 +13,21 @@ abstract class CoroutineConnectable<I, O>(
   private val dispatcher: CoroutineDispatcher
 ) : Connectable<I, O>, CoroutineScope {
 
-  private val parentJob = SupervisorJob()
+  private val supervisorJob = SupervisorJob()
   override val coroutineContext: CoroutineContext
-    get() = parentJob + dispatcher
+    get() = supervisorJob + dispatcher
 
   abstract suspend fun handler(effect: I, dispatchEvent: (O) -> Unit)
 
   override fun connect(output: Consumer<O>): Connection<I> {
-    val job = Job()
-    val scope = CoroutineScope(coroutineContext + job)
 
     return object : Connection<I> {
       override fun accept(input: I) {
-        scope.launch { handler(input, output::accept) }
+        launch { handler(input, output::accept) }
       }
 
       override fun dispose() {
-        job.cancel()
-        parentJob.cancel()
+        supervisorJob.cancel()
       }
     }
   }
