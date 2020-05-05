5a249f6408d5828a81f526a033b27a6d6b01e7c1
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-05-05 20:48:52 +0530

    Receive shared text from other apps to be used as notification content

--- app/src/main/java/dev/sasikanth/pinnit/ShortcutReceiverActivity.kt
+++ app/src/main/java/dev/sasikanth/pinnit/ShortcutReceiverActivity.kt
@@ -1,5 +1,6 @@
 package dev.sasikanth.pinnit
 
+import android.content.Intent.EXTRA_TEXT
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavDeepLinkBuilder
@@ -11,12 +12,14 @@ class ShortcutReceiverActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
+    val receivedText = intent.getStringExtra(EXTRA_TEXT)
+
     // Launch the deep link to editor page
     NavDeepLinkBuilder(this)
       .setComponentName(MainActivity::class.java)
       .setDestination(R.id.editorScreen)
       .setGraph(R.navigation.main_nav_graph)
-      .setArguments(EditorScreenArgs().toBundle())
+      .setArguments(EditorScreenArgs(notificationContent = receivedText).toBundle())
       .createTaskStackBuilder()
       .startActivities()
 
