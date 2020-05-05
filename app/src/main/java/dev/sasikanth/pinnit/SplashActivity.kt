f20f7c3a46c0192463091aa351288ed18eb3f5dc
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-05-05 21:14:27 +0530

    Use `AnimatedVectorDrawableCompat` in `SplashActivity`
    `AnimatedVectorDrawable` uses different variants depending on the API version. For API 21 & 23 it used `AnimatedVectorDrawableCompat` where as for API 25 and above it used `AnimatedVectorDrawable`. Because I was casting the drawable gotten from the `ImageView` to `AnimatedVectorDrawable` it crashes on API 23. In order to fix that, I am constructing the AVD at runtime using `AnimatedVectorDrawableCompat`.
    
    For more information:
    https://medium.com/androiddevelopers/re-animation-7869722af206

--- app/src/main/java/dev/sasikanth/pinnit/SplashActivity.kt
+++ app/src/main/java/dev/sasikanth/pinnit/SplashActivity.kt
@@ -1,19 +1,19 @@
 package dev.sasikanth.pinnit
 
 import android.content.Intent
-import android.graphics.drawable.Animatable2
-import android.graphics.drawable.AnimatedVectorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.doOnLayout
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import dev.sasikanth.pinnit.activity.MainActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
-  private val animationCallback = object : Animatable2.AnimationCallback() {
+  private val animationCallback = object : Animatable2Compat.AnimationCallback() {
     override fun onAnimationEnd(drawable: Drawable?) {
       super.onAnimationEnd(drawable)
       val intent = Intent(this@SplashActivity, MainActivity::class.java)
@@ -22,7 +22,7 @@ class SplashActivity : AppCompatActivity() {
     }
   }
 
-  private lateinit var animatedWelcomeImage: AnimatedVectorDrawable
+  private var animatedWelcomeImage: AnimatedVectorDrawableCompat? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -30,15 +30,17 @@ class SplashActivity : AppCompatActivity() {
 
     rootView.systemUiVisibility += View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 
-    animatedWelcomeImage = welcomeImage.drawable as AnimatedVectorDrawable
+    animatedWelcomeImage = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_pin_welcome)
+
+    welcomeImage.setImageDrawable(animatedWelcomeImage)
     welcomeImage.doOnLayout {
-      animatedWelcomeImage.start()
-      animatedWelcomeImage.registerAnimationCallback(animationCallback)
+      animatedWelcomeImage?.start()
+      animatedWelcomeImage?.registerAnimationCallback(animationCallback)
     }
   }
 
   override fun onDestroy() {
-    animatedWelcomeImage.unregisterAnimationCallback(animationCallback)
+    animatedWelcomeImage?.unregisterAnimationCallback(animationCallback)
     super.onDestroy()
   }
 }
