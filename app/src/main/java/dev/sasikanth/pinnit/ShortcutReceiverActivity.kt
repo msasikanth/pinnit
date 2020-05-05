28ea2278a9775a5de62856189876fab24d46886a
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-05-05 07:23:50 +0530

    Add app shortcut for create

---
+++ app/src/main/java/dev/sasikanth/pinnit/ShortcutReceiverActivity.kt
@@ -0,0 +1,26 @@
+package dev.sasikanth.pinnit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavDeepLinkBuilder
+import dev.sasikanth.pinnit.activity.MainActivity
+import dev.sasikanth.pinnit.editor.EditorScreenArgs
+
+class ShortcutReceiverActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    // Launch the deep link to editor page
+    NavDeepLinkBuilder(this)
+      .setComponentName(MainActivity::class.java)
+      .setDestination(R.id.editorScreen)
+      .setGraph(R.navigation.main_nav_graph)
+      .setArguments(EditorScreenArgs(notificationUuid = null).toBundle())
+      .createTaskStackBuilder()
+      .startActivities()
+
+    // finish the activity once necessary actions are performed
+    finish()
+  }
+}
