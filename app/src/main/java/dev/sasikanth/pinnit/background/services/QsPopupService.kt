09098bc8f3c75c212887396efa08947493f6bc70
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-05-09 21:29:40 +0530

    Launch `QsPopupActivity` when QS tile is clicked

--- app/src/main/java/dev/sasikanth/pinnit/background/services/QsPopupService.kt
+++ app/src/main/java/dev/sasikanth/pinnit/background/services/QsPopupService.kt
@@ -5,16 +5,16 @@ import android.os.Build
 import android.service.quicksettings.Tile.STATE_INACTIVE
 import android.service.quicksettings.TileService
 import androidx.annotation.RequiresApi
-import dev.sasikanth.pinnit.activity.MainActivity
+import dev.sasikanth.pinnit.qspopup.QsPopupActivity
 
 @RequiresApi(Build.VERSION_CODES.N)
 class QsPopupService : TileService() {
 
   override fun onClick() {
-    val mainIntent = Intent(this, MainActivity::class.java).apply {
+    val intent = Intent(this, QsPopupActivity::class.java).apply {
       addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     }
-    startActivityAndCollapse(mainIntent)
+    startActivityAndCollapse(intent)
   }
 
   override fun onTileAdded() {
