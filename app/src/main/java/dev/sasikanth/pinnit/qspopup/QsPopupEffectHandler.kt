03176a495c0cb08fe486cc5a6ec6d509aed0f3fa
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-05-09 21:05:25 +0530

    Handle `ToggleNotificationPinStatus` effect is QS popup

--- app/src/main/java/dev/sasikanth/pinnit/qspopup/QsPopupEffectHandler.kt
+++ app/src/main/java/dev/sasikanth/pinnit/qspopup/QsPopupEffectHandler.kt
@@ -6,12 +6,14 @@ import com.squareup.inject.assisted.AssistedInject
 import dev.sasikanth.pinnit.mobius.CoroutineConnectable
 import dev.sasikanth.pinnit.notifications.NotificationRepository
 import dev.sasikanth.pinnit.utils.DispatcherProvider
+import dev.sasikanth.pinnit.utils.notification.NotificationUtil
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 class QsPopupEffectHandler @AssistedInject constructor(
   private val dispatcherProvider: DispatcherProvider,
   private val notificationRepository: NotificationRepository,
+  private val notificationUtil: NotificationUtil,
   @Assisted private val viewEffectConsumer: Consumer<QsPopupViewEffect>
 ) : CoroutineConnectable<QsPopupEffect, QsPopupEvent>(dispatcherProvider.main) {
 
@@ -24,6 +26,8 @@ class QsPopupEffectHandler @AssistedInject constructor(
     when (effect) {
       LoadNotifications -> loadNotifications(dispatchEvent)
       is OpenNotificationEditor -> viewEffectConsumer.accept(OpenNotificationEditorViewEffect(effect.notification))
+
+      is ToggleNotificationPinStatus -> toggleNotificationPinStatus(effect)
     }
   }
 
@@ -33,4 +37,21 @@ class QsPopupEffectHandler @AssistedInject constructor(
       .onEach { dispatchEvent(NotificationsLoaded(it)) }
       .launchIn(this)
   }
+
+  private suspend fun toggleNotificationPinStatus(effect: ToggleNotificationPinStatus) {
+    val notification = effect.notification
+    // We are doing is before to avoid waiting for the I/O
+    // actions to be completed and also to avoid changing the
+    // `toggleNotificationPinStatus` method to return the
+    // notification.
+    if (notification.isPinned) {
+      // If already pinned, then dismiss the notification
+      notificationUtil.dismissNotification(notification)
+    } else {
+      // If already is not pinned, then show the notification and pin it
+      notificationUtil.showNotification(notification)
+    }
+    notificationRepository.toggleNotificationPinStatus(notification)
+  }
+
 }
