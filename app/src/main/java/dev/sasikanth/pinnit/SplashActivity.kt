0be0f956957c1e75c5cd5098b7f1585674f84a49
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-06-20 09:26:12 +0530

    Update insetter dependency

--- app/src/main/java/dev/sasikanth/pinnit/SplashActivity.kt
+++ app/src/main/java/dev/sasikanth/pinnit/SplashActivity.kt
@@ -3,11 +3,11 @@ package dev.sasikanth.pinnit
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.doOnLayout
 import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import dev.chrisbanes.insetter.setEdgeToEdgeSystemUiFlags
 import dev.sasikanth.pinnit.activity.MainActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -28,7 +28,7 @@ class SplashActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_splash)
 
-    rootView.systemUiVisibility += View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    rootView.setEdgeToEdgeSystemUiFlags()
 
     animatedWelcomeImage = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_pin_welcome)
 
