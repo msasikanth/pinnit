733692fffbacd03092c71e1d149eb3c0a943c36f
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-09-11 09:59:22 +0530

    Remove `ValueChangedCallback`

--- app/src/main/java/dev/sasikanth/pinnit/editor/EditorScreenUiRender.kt
+++ app/src/main/java/dev/sasikanth/pinnit/editor/EditorScreenUiRender.kt
@@ -1,30 +1,24 @@
 package dev.sasikanth.pinnit.editor
 
-import dev.sasikanth.pinnit.utils.ValueChangedCallback
-
 class EditorScreenUiRender(private val ui: EditorScreenUi) {
 
-  private val valueChangedCallback = ValueChangedCallback<EditorScreenModel>()
-
   fun render(model: EditorScreenModel) {
-    valueChangedCallback.pass(model) {
-      if (model.isNotificationPinned) {
-        ui.renderSaveActionButtonText()
-      } else {
-        ui.renderSaveAndPinActionButtonText()
-      }
+    if (model.isNotificationPinned) {
+      ui.renderSaveActionButtonText()
+    } else {
+      ui.renderSaveAndPinActionButtonText()
+    }
 
-      if (model.isNotificationLoaded) {
-        ui.showDeleteButton()
-      } else {
-        ui.hideDeleteButton()
-      }
+    if (model.isNotificationLoaded) {
+      ui.showDeleteButton()
+    } else {
+      ui.hideDeleteButton()
+    }
 
-      if (!model.title.isNullOrBlank()) {
-        ui.enableSave()
-      } else {
-        ui.disableSave()
-      }
+    if (!model.title.isNullOrBlank()) {
+      ui.enableSave()
+    } else {
+      ui.disableSave()
     }
   }
 }
