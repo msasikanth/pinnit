b838ce36d03e42fc3277ff0d452671e7179647d4
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2021-04-11 10:21:12 +0530

    Remove `PinnitPreferences`

--- app/src/main/java/dev/sasikanth/pinnit/di/PreferencesModule.kt
+++ app/src/main/java/dev/sasikanth/pinnit/di/PreferencesModule.kt
@@ -1,9 +1,7 @@
 package dev.sasikanth.pinnit.di
 
 import android.app.Application
-import android.content.SharedPreferences
 import androidx.datastore.core.DataStore
-import androidx.preference.PreferenceManager
 import dagger.Module
 import dagger.Provides
 import dev.sasikanth.pinnit.data.preferences.AppPreferences
@@ -11,12 +9,6 @@ import dev.sasikanth.pinnit.data.preferences.appPreferencesStore
 
 @Module
 object PreferencesModule {
-
-  @AppScope
-  @Provides
-  fun providesAppPreferences(application: Application): SharedPreferences {
-    return PreferenceManager.getDefaultSharedPreferences(application)
-  }
 
   @AppScope
   @Provides
