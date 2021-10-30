08b66b921efaff527eae67cbbb1a990d2b3892ee
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-10-30 13:41:08 +0530

    Install `PreferencesModule` in `SingletonComponent`

--- app/src/main/java/dev/sasikanth/pinnit/di/PreferencesModule.kt
+++ app/src/main/java/dev/sasikanth/pinnit/di/PreferencesModule.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import dev.sasikanth.pinnit.data.preferences.AppPreferences
 import dev.sasikanth.pinnit.data.preferences.appPreferencesStore
 import javax.inject.Singleton
 
+@InstallIn(SingletonComponent::class)
 @Module
 object PreferencesModule {
 
