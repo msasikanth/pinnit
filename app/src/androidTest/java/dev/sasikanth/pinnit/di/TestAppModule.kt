04ad525e898ddf566449a27c6f64d2c730d9f5e6
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-10-30 13:48:07 +0530

    Provide default system zone in `TestAppModule`

--- app/src/androidTest/java/dev/sasikanth/pinnit/di/TestAppModule.kt
+++ app/src/androidTest/java/dev/sasikanth/pinnit/di/TestAppModule.kt
@@ -14,6 +14,7 @@ import dev.sasikanth.pinnit.utils.TestUserClock
 import dev.sasikanth.pinnit.utils.TestUtcClock
 import dev.sasikanth.pinnit.utils.UserClock
 import dev.sasikanth.pinnit.utils.UtcClock
+import java.time.ZoneId
 import javax.inject.Singleton
 
 @TestInstallIn(
@@ -53,4 +54,8 @@ object TestAppModule {
   @Singleton
   @Provides
   fun providesDispatcherProvider(): DispatcherProvider = CoroutineDispatcherProvider()
+
+  @Singleton
+  @Provides
+  fun providesSystemDefaultZone(): ZoneId = ZoneId.systemDefault()
 }
