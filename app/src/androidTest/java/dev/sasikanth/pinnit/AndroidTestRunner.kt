a369afe7235da3e57ee546585548917effd37097
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-10-30 13:31:55 +0530

    Use `HiltTestApplication` in `AndroidTestRunner`

--- app/src/androidTest/java/dev/sasikanth/pinnit/AndroidTestRunner.kt
+++ app/src/androidTest/java/dev/sasikanth/pinnit/AndroidTestRunner.kt
@@ -3,9 +3,10 @@ package dev.sasikanth.pinnit
 import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
 
 class AndroidTestRunner : AndroidJUnitRunner() {
   override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
-    return super.newApplication(cl, TestPinnitApp::class.java.name, context)
+    return super.newApplication(cl, HiltTestApplication::class.java.name, context)
   }
 }
