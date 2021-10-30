c3db33ce888a59217126ef19751e50da43ed3cdb
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-10-30 13:40:37 +0530

    Install `NotificationModule` in `SingletonComponent`

--- app/src/main/java/dev/sasikanth/pinnit/notifications/NotificationModule.kt
+++ app/src/main/java/dev/sasikanth/pinnit/notifications/NotificationModule.kt
@@ -2,10 +2,13 @@ package dev.sasikanth.pinnit.notifications
 
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import dev.sasikanth.pinnit.data.AppDatabase
 import dev.sasikanth.pinnit.data.PinnitNotification
 import javax.inject.Singleton
 
+@InstallIn(SingletonComponent::class)
 @Module
 object NotificationModule {
 
