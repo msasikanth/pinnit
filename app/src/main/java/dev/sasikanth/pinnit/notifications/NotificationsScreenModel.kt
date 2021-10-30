1115a8c27a243e99e0c8571a713af85630a83c15
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-10-30 14:49:43 +0530

    Implement `NotificationsViewModel`

--- app/src/main/java/dev/sasikanth/pinnit/notifications/NotificationsScreenModel.kt
+++ app/src/main/java/dev/sasikanth/pinnit/notifications/NotificationsScreenModel.kt
@@ -1,10 +1,13 @@
 package dev.sasikanth.pinnit.notifications
 
+import android.os.Parcelable
 import dev.sasikanth.pinnit.data.PinnitNotification
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class NotificationsScreenModel(
   val notifications: List<PinnitNotification>?
-) {
+) : Parcelable {
 
   companion object {
     fun default() = NotificationsScreenModel(notifications = null)
---
+++ app/src/main/java/dev/sasikanth/pinnit/notifications/NotificationsScreenViewModel.kt
@@ -0,0 +1,36 @@
+package dev.sasikanth.pinnit.notifications
+
+import androidx.lifecycle.SavedStateHandle
+import com.spotify.mobius.Mobius
+import com.spotify.mobius.android.MobiusLoopViewModel
+import com.spotify.mobius.runners.WorkRunner
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.sasikanth.pinnit.utils.MAX_VIEW_EFFECTS_QUEUE_SIZE
+import javax.inject.Inject
+
+@HiltViewModel
+class NotificationsScreenViewModel @Inject constructor(
+  private val savedStateHandle: SavedStateHandle,
+  update: NotificationsScreenUpdate,
+  init: NotificationsScreenInit,
+  effectHandlerFactory: NotificationsScreenEffectHandler.Factory,
+  workRunner: WorkRunner
+) : MobiusLoopViewModel<NotificationsScreenModel, NotificationsScreenEvent, NotificationsScreenEffect, NotificationScreenViewEffect>(
+  { viewEffectsConsumer ->
+    Mobius.loop(update, effectHandlerFactory.create(viewEffectsConsumer))
+  },
+  savedStateHandle.get<NotificationsScreenModel>(MODEL_KEY) ?: NotificationsScreenModel.default(),
+  init,
+  workRunner,
+  MAX_VIEW_EFFECTS_QUEUE_SIZE
+) {
+
+  companion object {
+    private const val MODEL_KEY = "NOTIFICATIONS_SCREEN_MODEL"
+  }
+
+  override fun onClearedInternal() {
+    savedStateHandle.set(MODEL_KEY, model)
+    super.onClearedInternal()
+  }
+}
