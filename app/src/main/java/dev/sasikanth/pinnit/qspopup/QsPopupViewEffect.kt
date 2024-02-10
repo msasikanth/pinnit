bf02b0fc757deb4f5344a682d4229fe43542ef89
Sasikanth Miriyampalli <hello@sasikanth.dev>
2024-02-10 11:40:36 +0530

    Remove `QsPopup`
    This is no longer recommended by Android and is not working from Android 34 onwards. So, removing it.

--- app/src/main/java/dev/sasikanth/pinnit/qspopup/QsPopupViewEffect.kt
+++
@@ -1,7 +0,0 @@
-package dev.sasikanth.pinnit.qspopup
-
-import dev.sasikanth.pinnit.data.PinnitNotification
-
-sealed class QsPopupViewEffect
-
-data class OpenNotificationEditorViewEffect(val notification: PinnitNotification) : QsPopupViewEffect()
